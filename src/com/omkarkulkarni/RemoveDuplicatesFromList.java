a894edf16a019ca89087b0af794428e8e978a152
OkayKul <omkar15525@gmail.com>
2021-10-10 21:56:23 +0530

    Added Second Approach

--- src/com/omkarkulkarni/RemoveDuplicatesFromList.java
+++ src/com/omkarkulkarni/RemoveDuplicatesFromList.java
@@ -2,7 +2,9 @@ package com.omkarkulkarni;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 //Remove Duplicated from ArrayList
@@ -20,9 +22,23 @@ public class RemoveDuplicatesFromList {
 		List<Integer> resultantArray = listNumbers.stream()
 				.distinct()
 				.collect(Collectors.toList());
-		System.out.println("Arrays after removing duplicated through lambda functions:: ");
+//		System.out.println("Arrays after removing duplicated through lambda functions:: ");
 		
-		printArray(resultantArray);
+//		printArray(resultantArray);
+		
+		//Approach 2 //
+		//Set interface does not allow duplicates 
+		
+		Set<Integer> setOfIntegers = new LinkedHashSet<Integer>();
+		//Add all the numbers to the set
+		setOfIntegers.addAll(listNumbers);
+		// Clear the original list
+		listNumbers.clear();
+		// add numbers from set to original list
+		listNumbers.addAll(setOfIntegers);
+		
+		//print the sorted array
+		printArray(listNumbers);
 	}
 
 	private static void printArray(List<Integer> listNumbers) {
